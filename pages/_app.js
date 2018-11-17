c7368a8d443e925dcc506e3fa3d4ffb5f6bf187b
MrOrz <johnsonliang7@gmail.com>
2018-11-18 02:59:07 +0800

    Establish basic material-ui style

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import withApolloClient from '../lib/with-apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <Container>
+        <CssBaseline />
         <ApolloProvider client={apolloClient}>
           <Component {...pageProps} />
         </ApolloProvider>
---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          <style>
+            {`
+              body {
+                font-family: 蘋方-繁, PingFang TC, 思源黑體, Source Han Sans, Noto Sans CJK TC, sans-serif;
+              }
+            `}
+          </style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
