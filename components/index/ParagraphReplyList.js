a1eb243f62af839822e664e7c1c451bd47f31437
Johnson Liang <johnson.liang@appier.com>
2018-12-07 22:52:06 +0800

    Styling ParagraphReplyList

--- components/index/ParagraphReplyList.js
+++ components/index/ParagraphReplyList.js
@@ -1,4 +1,10 @@
 import gql from 'graphql-tag';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import { Link } from '../../routes';
+
 import { Query } from 'react-apollo';
 
 const LIST_ARTICLES = gql`
@@ -19,6 +25,40 @@ const LIST_ARTICLES = gql`
   }
 `;
 
+const ParagraphBubble = withStyles({
+  root: {
+    padding: 8,
+    marginBottom: 8,
+  },
+})(Card);
+
+const ReplyBubble = withStyles({
+  root: {
+    padding: 8,
+    backgroundColor: purple[400],
+    color: purple[50],
+  },
+})(Card);
+
+function ParagraphReply({ paragraph, reply }) {
+  return (
+    <Link route="article" params={{ id: paragraph.article.id }}>
+      <a className="paragraph-reply">
+        <ParagraphBubble>
+          <Typography color="textSecondary">{paragraph.text}</Typography>
+        </ParagraphBubble>
+        <ReplyBubble>{reply.text}</ReplyBubble>
+        <style jsx>{`
+          .paragraph-reply {
+            margin: 32px 8px;
+            text-decoration: none;
+          }
+        `}</style>
+      </a>
+    </Link>
+  );
+}
+
 function ParagraphReplyList() {
   return (
     <Query query={LIST_ARTICLES}>
@@ -33,12 +73,14 @@ function ParagraphReplyList() {
 
         return (
           <section>
-            paragraphReplies
-            {data.paragraphReplies.map(paragraphReply => (
-              <div key={paragraphReply.id}>
-                {JSON.stringify(paragraphReply)}
-              </div>
+            {data.paragraphReplies.map(({ id, paragraph, reply }) => (
+              <ParagraphReply key={id} paragraph={paragraph} reply={reply} />
             ))}
+            <style jsx>{`
+              section {
+                padding: 8px;
+              }
+            `}</style>
           </section>
         );
       }}
