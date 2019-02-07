0965b693ed7c2edf6759a23b06649457372920d4
MrOrz <johnsonliang7@gmail.com>
2019-02-07 23:16:30 +0800

    Basic style on ArticleSearch

--- components/search/ArticleSearch.js
+++ components/search/ArticleSearch.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import gql from 'graphql-tag';
+
 import { Query } from 'react-apollo';
+import { Link } from '../../routes';
 import { truncate, parseTag } from '../../lib/text';
+import { withStyles, Typography } from '@material-ui/core';
+
+const styles = theme => ({
+  container: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+
+  article: {
+    margin: `${theme.spacing.unit * 2}px 0`,
+  },
+
+  articleTitle: {
+    textDecoration: 'none',
+    '&:hover h6': {
+      textDecoration: 'underline',
+    },
+  },
+});
 
 const LIST_ARTICLES = gql`
   query listArticles($inText: String) {
@@ -14,21 +36,35 @@ const LIST_ARTICLES = gql`
           id
         }
       }
+      sources {
+        note
+      }
     }
   }
 `;
 
-function Article({ article: { text, _highlight } }) {
+function Article({ article: { id, text, _highlight, sources }, classes }) {
   const title = truncate(text.split('\n', 1)[0], {
     wordCount: 50,
     moreElem: '⋯⋯',
   });
 
+  const sourcesStr = `${sources[0] && sources[0].note} ${
+    sources.length > 1 ? `等 ${sources.length} 個來源` : ''
+  }`;
+
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{parseTag(_highlight, { fromElem: 'HIGHLIGHT', toElem: 'mark' })}</p>
-    </div>
+    <article className={classes.article}>
+      <Link route={`/article/${id}`}>
+        <a className={classes.articleTitle}>
+          <Typography variant="h6">{title}</Typography>
+          <Typography>{sourcesStr}</Typography>
+        </a>
+      </Link>
+      <Typography tag="p" variant="body2">
+        {parseTag(_highlight, { fromElem: 'HIGHLIGHT', toElem: 'mark' })}
+      </Typography>
+    </article>
   );
 }
 
@@ -39,24 +75,24 @@ class ArticleSearch extends React.Component {
   };
 
   render() {
-    const { searchedText, articles } = this.props;
+    const { searchedText, articles, classes } = this.props;
 
     return (
-      <div>
-        <header>
+      <div className={classes.container}>
+        <Typography tag="header" variant="subtitle1">
           與「{truncate(searchedText, { wordCount: 10, moreElem: '⋯⋯' })}
           」相近的訊息有：
-        </header>
+        </Typography>
 
         {articles.map(article => (
-          <Article key={article.id} article={article} />
+          <Article key={article.id} article={article} classes={classes} />
         ))}
       </div>
     );
   }
 }
 
-function ArticleSearchWrapper({ searchedText }) {
+function ArticleSearchWrapper({ searchedText, classes }) {
   return (
     <Query query={LIST_ARTICLES} variables={{ inText: searchedText }}>
       {({ data, error, loading }) => {
@@ -67,11 +103,15 @@ function ArticleSearchWrapper({ searchedText }) {
           return <p>Loading...</p>;
         }
         return (
-          <ArticleSearch searchedText={searchedText} articles={data.articles} />
+          <ArticleSearch
+            searchedText={searchedText}
+            articles={data.articles}
+            classes={classes}
+          />
         );
       }}
     </Query>
   );
 }
 
-export default ArticleSearchWrapper;
+export default withStyles(styles)(ArticleSearchWrapper);
